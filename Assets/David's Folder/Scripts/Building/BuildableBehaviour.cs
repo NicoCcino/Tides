d66165e96cc00d195ac7b01557379686d245ade3
DavidHamon <davidhamon3@gmail.com>
2026-05-16 16:25:13 +0200

    feat :WIP on building architecture

--- Assets/David's Folder/Scripts/Building/BuildableBehaviour.cs
+++ Assets/David's Folder/Scripts/Building/BuildableBehaviour.cs
@@ -1,17 +1,32 @@
-using IFCE.ValueTracking.UX;
-using Unity.VisualScripting;
+using Tides.Resources;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BuildableBehaviour : MonoBehaviour
+public class BuildableBehaviour : MonoBehaviour, IPointerDownHandler
 {
+
+    [SerializeField] private int buildingCost = 25;
+
     [SerializeField] private GameObject buildableGameObject;
+    [SerializeField] private GameObject previewGameObject;
     [SerializeField] private GameObject buildedGameObject;
+    [SerializeField] private float buildingTime;
+    [SerializeField] private ShaderFloatUpdater visualProgressEffect;
 
+    [SerializeField, Range(0, 1)] private float buildingProgress = 0.0f;
 
+    private bool assignedBuilding = false;
+
+    public void TickUpdateProgress()
+    {
+        buildingProgress += (1 / buildingTime);
+        UpdateProgress(buildingProgress);
+    }
 
     private void UpdateProgress(float amount)
     {
-
+        visualProgressEffect.UpdateVisual(amount);
+        previewGameObject.SetActive(amount <= 0);
         if (amount >= 1.0f)
         {
             SpawnBuilding();
@@ -21,5 +36,34 @@ public class BuildableBehaviour : MonoBehaviour
     {
         buildedGameObject.SetActive(true);
         buildableGameObject.SetActive(false);
+    }
+    public void DestroyBuilding()
+    {
+        buildedGameObject.SetActive(false);
+        buildableGameObject.SetActive(true);
+        assignedBuilding = false;
+    }
+    public void CancelBuilding()
+    {
+        assignedBuilding = false;
+        ResourcesManager.Instance.AddWood(buildingCost);
+        UpdateProgress(0f);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        if (assignedBuilding == true)
+        {
+            //DEBUG ONLY
+            TickUpdateProgress();
+            //
+            return;
+        }
+        if ((ResourcesManager.Instance.WoodResource as IResource).CanConsume(buildingCost))
+        {
+            ResourcesManager.Instance.ConsumeWood(buildingCost);
+            assignedBuilding = true;
+        }
     }
 }
