8563785747312d221e84598868bf2d19384a47fe
DavidHamon <davidhamon3@gmail.com>
2026-05-17 15:14:02 +0200

    fix : Ship buildable , Canvas_main

--- Assets/David's Folder/Scripts/JobableBehaviour.cs
+++ Assets/David's Folder/Scripts/JobableBehaviour.cs
@@ -4,11 +4,13 @@ using UnityEngine.EventSystems;
 
 public class JobableBehaviour : MonoBehaviour, IPointerDownHandler
 {
+    [SerializeField] private bool ControlsUIDisplayal = true;
     [SerializeField] public GameObject JobableControlsGameObject;
     public IJobProvider Jobable { get; protected set; }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (!ControlsUIDisplayal) return;
         JobableControlsGameObject.SetActive(true);
     }
 
